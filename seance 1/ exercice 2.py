c67de9626baa2ed1fd6fda4c51764573852debf2
Jie <jiefan0623@yahoo.com>
2023-11-24 16:21:13 +0100

    change exercice 2

--- seance 1/ exercice 2.py
+++ seance 1/ exercice 2.py
@@ -6,12 +6,12 @@ print("chemin du dossier courant:" + chemin_courant)
 nom_dir_courant = os.path.basename(chemin_courant)
 print("nom du répertoire courant:" + nom_dir_courant)
 
-nouveau_nom_dir= "nouveau_repertoire"
-os.makedirs(nouveau_nom_dir)
-print("Créer un répertoire :" + nouveau_nom_dir)
+nouveau_nom_rep= "nouveau_repertoire"
+os.makedirs(nouveau_nom_rep)
+print("Créer un répertoire :" + nouveau_nom_rep)
 
-os.rmdir(nouveau_nom_dir)
-print("Supprimer le répertoire:" + nouveau_nom_dir)
+os.rmdir(nouveau_nom_rep)
+print("Supprimer le répertoire:" + nouveau_nom_rep)
 
 lister = os.listdir(chemin_courant)
 print("Lister les fichiers et les dossiers" + str(lister))
